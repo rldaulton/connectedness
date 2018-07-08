dc1e8f20dfa56df0ce1bbb0e3007b45f2bc42646
Diego Quimbo <dquimbo22@gmail.com>
2018-07-08 10:30:38 -0300

    Migrate code to Swift 4.0

--- connectedness.swift
+++ connectedness.swift
@@ -5,10 +5,6 @@
 //  Created by Ryan Daulton on 10/4/16.
 //  Copyright © 2016 Ryan Daulton. All rights reserved.
 //  
-//  Code derived from multiple StackOverflow submissions by users:
-//      Rob: http://stackoverflow.com/users/1271826/rob
-//      Leo Dabus: http://stackoverflow.com/users/2303865/leo-dabus
-//
 
 
 import Foundation
@@ -17,15 +13,16 @@ import SystemConfiguration
 public func connectedToNetwork() {
     
     var zeroAddress = sockaddr_in()
-    zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+    zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
     zeroAddress.sin_family = sa_family_t(AF_INET)
     
-    guard let defaultRouteReachability = withUnsafePointer(&zeroAddress, {
-        SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+    guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+        $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+            SCNetworkReachabilityCreateWithAddress(nil, $0)
+        }
     }) else {
         print("Network Unreachable - Zero Address")
         return
-        
     }
     
     var flags : SCNetworkReachabilityFlags = []
@@ -34,31 +31,34 @@ public func connectedToNetwork() {
         return
     }
     
-    let isReachable = flags.contains(.Reachable)
-    let needsConnection = flags.contains(.ConnectionRequired)
-    connectedSpeed().testDownloadSpeedWithTimout(5.0) { (megabytesPerSecond, error) -> () in
-        if ((error == nil) && isReachable && !needsConnection){
-            if megabytesPerSecond!>=0.05{
-            print("\n       | Connected |")
-            print("=======Network Speed=======\nMBps: \(megabytesPerSecond!)\nerror: \(error)\n=======-------------=======\n")
+    let isReachable = flags.contains(.reachable)
+    let needsConnection = flags.contains(.connectionRequired)
+    
+    connectedSpeed().testDownloadSpeedWithTimout(timeout: 5.0) { (megabytesPerSecond, error) -> () in
+        
+        if ((error == nil) && isReachable && !needsConnection) {
+            if megabytesPerSecond!>=0.05 {
+                print("\n       | Connected |")
+                print("=======Network Speed=======\nMBps: \(megabytesPerSecond!)\nerror: \(String(describing: error))\n=======-------------=======\n")
             }else{
                 print("\n       ? Connected ?")
-                print("*======| WEAK SIGNAL |======*\nMBps: \(megabytesPerSecond!)\nerror: \(error)\n*=======-------------=======*\n")
+                print("*======| WEAK SIGNAL |======*\nMBps: \(megabytesPerSecond!)\nerror: \(String(describing: error))\n*=======-------------=======*\n")
             }
         }else{
-            print("NETWORK ERROR: \(error)")
+            print("NETWORK ERROR: \(String(describing: error))")
+            return
         }
     }
     
 }
 
-public class connectedSpeed: NSObject,NSURLSessionDelegate, NSURLSessionDataDelegate {
+public class connectedSpeed: NSObject, URLSessionDelegate, URLSessionDataDelegate {
     ////
     
     var startTime: CFAbsoluteTime!
     var stopTime: CFAbsoluteTime!
     var bytesReceived: Int!
-    var speedTestCompletionHandler: ((megabytesPerSecond: Double?, error: NSError?) -> ())!
+    var speedTestCompletionHandler: ((_ megabytesPerSecond: Double?, _ error: Error?) -> ())!
     
     /// Test speed of download
     ///
@@ -73,34 +73,36 @@ public class connectedSpeed: NSObject,NSURLSessionDelegate, NSURLSessionDataDele
     /// - note:                          Note, the timeout parameter doesn't have to be enough to download the entire
     ///                                  resource, but rather just sufficiently long enough to measure the speed of the download.
     
-    public func testDownloadSpeedWithTimout(timeout: NSTimeInterval, completionHandler:(megabytesPerSecond: Double?, error: NSError?) -> ()) {
-        let url = NSURL(string: "http://insert.your.site.here/yourfile")!
+    public func testDownloadSpeedWithTimout(timeout: TimeInterval, completionHandler: @escaping (_ megabytesPerSecond: Double?, _ error: Error?) -> ()) {
+        let url =  URL(string: "http://insert.your.site.here/yourfile")!
         
         startTime = CFAbsoluteTimeGetCurrent()
         stopTime = startTime
         bytesReceived = 0
         speedTestCompletionHandler = completionHandler
         
-        let configuration = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+        
+        let configuration = URLSessionConfiguration.ephemeral
         configuration.timeoutIntervalForResource = timeout
-        let session = NSURLSession(configuration: configuration, delegate: self, delegateQueue: nil)
-        session.dataTaskWithURL(url).resume()
+        let session = URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
+        session.dataTask(with: url).resume()
+
     }
     
-    public func URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didReceiveData data: NSData){
-        bytesReceived! += data.length
+    public func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data){
+        bytesReceived! += data.count
         stopTime = CFAbsoluteTimeGetCurrent()
     }
     
-    public func URLSession(session: NSURLSession, task: NSURLSessionTask, didCompleteWithError error: NSError?) {
+    public func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
         let elapsed = stopTime - startTime
-        guard elapsed != 0 && (error == nil || (error?.domain == NSURLErrorDomain && error?.code == NSURLErrorTimedOut)) else {
-            speedTestCompletionHandler(megabytesPerSecond: nil, error: error)
+        guard elapsed != 0 && (error == nil || (error?._domain == NSURLErrorDomain && error?._code == NSURLErrorTimedOut)) else {
+            speedTestCompletionHandler(nil, error)
             return
         }
         
         let speed = elapsed != 0 ? Double(bytesReceived) / elapsed / 1024.0 / 1024.0 : -1
-        speedTestCompletionHandler(megabytesPerSecond: speed, error: nil)
+        speedTestCompletionHandler(speed, nil)
     }
     
     /////
